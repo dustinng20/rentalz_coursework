6ee3aa87241033afe27cd9e42d293af18b249407
Dustin <dungndgbd18501@fpt.edu.vn>
2021-11-07 15:46:38 +0700

    AddLayout: not finished

---
+++ app/src/main/java/com/example/rentalz_coursework/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.rentalz_coursework;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.homepage);
+    }
+}
